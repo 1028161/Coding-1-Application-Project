58a6c8f0e505aac8be589a13026f992d0f35019d
S1009003 <S1009003@student.knoxschools.org>
2026-05-13 14:18:17 +0000

    edit finished??

--- database.py
+++ database.py
@@ -16,7 +16,8 @@ def init_db():
     """)
     conn.execute("""
         CREATE TABLE IF NOT EXISTS twisters (
-            title TEXT PRIMARY KEY,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
             description TEXT
         )
     """)
